5c305a014cd50f41164d261741ba7cb21d473f45
Jahid Hasan <jh3495689@gmail.com>
2023-05-17 02:44:35 +0600

    booking api added a database

--- index.js
+++ index.js
@@ -65,6 +65,16 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/bookings', async(req, res)=> {
+            console.log(req.query.email)
+            let query = {}
+            if(req.query?.email){
+                query ={email: req.query.email}
+            }
+            const result = await bookingCollection.find(query).toArray()
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
