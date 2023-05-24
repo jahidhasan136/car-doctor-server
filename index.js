202154b22e53786d6270a669e3d2aa4ff35cee46
Jahid Hasan <jh3495689@gmail.com>
2023-05-24 10:28:02 +0600

    added jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -35,10 +36,21 @@ async function run() {
         await client.connect();
 
 
-
-
         const carCollection = client.db("carDB").collection("services");
         const bookingCollection = client.db("carDB").collection("bookings")
+
+
+    
+        console.log(process.env.ACCESS_TOKEN_SECRET)
+        
+        //jwt
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            });
+            res.send({token})
+        })
 
 
         app.get('/services', async (req, res) => {
@@ -49,31 +61,54 @@ async function run() {
 
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const options = {
 
-                projection: {  title: 1, price: 1, img: 1, service_id: 1 },
+                projection: { title: 1, price: 1, img: 1, service_id: 1 },
 
-              };
+            };
             const result = await carCollection.findOne(query, options)
             res.send(result)
         })
 
-        app.post('/bookings', async(req, res) => {
+        app.post('/bookings', async (req, res) => {
             const booking = req.body
             const result = await bookingCollection.insertOne(booking)
             res.send(result)
         })
 
-        app.get('/bookings', async(req, res)=> {
+        app.get('/bookings', async (req, res) => {
             console.log(req.query.email)
             let query = {}
-            if(req.query?.email){
-                query ={email: req.query.email}
+            if (req.query?.email) {
+                query = { email: req.query.email }
             }
             const result = await bookingCollection.find(query).toArray()
             res.send(result)
         })
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.patch('/bookings/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updateBooking = req.body
+            const updateDoc = {
+                $set: {
+                    status: updateBooking.status
+                },
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+
+
 
 
         // Send a ping to confirm a successful connection
